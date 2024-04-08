c52596daa723ae7d121a309afc908e535b8b615f
Golam Mahabub Redoy <148676251+gmhridu@users.noreply.github.com>
2024-04-08 13:59:09 +0600

    added images and data json

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
 const Root = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    document.title = getTitleFromPathname(location.pathname);
+  }, [location.pathname]);
+
+  // Function to generate title
+  const getTitleFromPathname = (pathname) => {
+    if (pathname === "/") {
+      return "NestQuest | Home";
+    } else {
+      return "NestQuest";
+    }
+  };
+
   return (
     <div>
-      <Outlet></Outlet>
+      <Outlet />
     </div>
   );
 };
