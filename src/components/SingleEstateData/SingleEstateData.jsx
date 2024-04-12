fd7590e002ccea3b11ba4d628e9bc921ff6dedb0
Golam Mahabub Redoy <148676251+gmhridu@users.noreply.github.com>
2024-04-12 23:53:01 +0600

    Enhanced single estate data by updating information based on ID for a more engaging presentation.

--- src/components/SingleEstateData/SingleEstateData.jsx
+++ src/components/SingleEstateData/SingleEstateData.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import estateData from '../../../public/data.json'
 import { BiArea, BiBath, BiBed } from 'react-icons/bi';
 
@@ -12,12 +12,14 @@ const SingleEstateData = () => {
   });
 
   
-  
+  const handleContactUs = (e) => {
+    e.preventDefault();
+  }
   
   return (
     <section>
       <div
-        className="container mx-auto min-h-[800px] mb-14
+        className="container mx-auto min-h-[800px] mb-14 my-10
       "
       >
         <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between">
@@ -58,11 +60,40 @@ const SingleEstateData = () => {
             </div>
             <div>{house?.description}</div>
           </div>
-          <div>
-            <div>
-              <img src={house?.agent?.image} alt="" />
+          <div className="flex-1 bg-white w-full mb-8 border border-gray-300 rounded-lg px-6 py-8">
+            <div className="flex items-center gap-x-4 mb-8">
+              <div className="w-20 h-20 p-1 border border-gray-300 rounded-full">
+                <img src={house?.agent?.image} alt="" />
+              </div>
+              <div className="font-bold text-lg">
+                <div>{house?.agent?.name}</div>
+                <Link to={""} className="text-violet-700 text-sm"></Link>
+              </div>
             </div>
-            <div></div>
+            {/* form */}
+            <form onSubmit={handleContactUs} className='flex flex-col gap-y-4'>
+              <input
+                className="border border-gray-300 focus:border-violet-700 outline-none rounded w-full px-4 h-14 text-sm"
+                type="text"
+                placeholder='Name*'
+              />
+              <input
+                className="border border-gray-300 focus:border-violet-700 outline-none rounded w-full px-4 h-14 text-sm"
+                type="text"
+                placeholder='Email*'
+              />
+              <input
+                className="border border-gray-300 focus:border-violet-700 outline-none rounded w-full px-4 h-14 text-sm"
+                type="text"
+                placeholder='Phone*'
+              />
+              <textarea className='border border-gray-300 focus:border-violet-700 outline-none resize-none rounded w-full p-4 h-36 text-sm text-gray-400
+             ' placeholder='Message&*'></textarea>
+              <div className='flex gap-x-2'>
+                <button className='btn btn-primary bg-violet-700 hover:bg-violet-800 text-white rounded p-4 text-sm transition'>Send Message</button>
+                <button className='btn btn-primary border border-violet-700 text-violet-700 hover:border-violet-500hover:text-violet-500 rounded p-4 text-sm transition'>Call</button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
